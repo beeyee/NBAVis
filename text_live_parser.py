d36418521ae5b8be9b5a5755ac12ed97fa543f86
beeyee <paula-bf@163.com>
2014-03-12 01:47:09 +0800

    cuccess to output a csv file

--- text_live_parser.py
+++ text_live_parser.py
@@ -3,6 +3,8 @@
 # Used to extract the information from a text live himl
 
 from bs4 import BeautifulSoup
+from pylab import *
+import csv
 
 # open the html file with BeautifulSoup.
 soup = BeautifulSoup(open('./files/text_live_file/mavs-76ers-20140221.html'))
@@ -16,23 +18,58 @@ for elem in divs:
     try:
 		if(elem['id'] == 'mediasportsplaybyplay'):
 		# print elem
-			play_by_play = elem[1];	
-		except KeyError:
-			continue
+			play_by_play = elem;	
+	except KeyError:
+		continue
 		
 # get the play part		
 for child in play_by_play:
     try:
 		if(child['class'] == ['bd']):
 			plays = child
-		except:
-			continue
+	except:
+		continue
 		
 for child in plays:
 	try:
 		if(child.name == 'dl'):
 			real_play = child
+	except:
+		continue
+				
+
+results = open('./results.csv', 'w')
+wholelist = list()
+arow = list();
+	for child in real_play:
+		try:
+			if(child['class'][0] == 'play'):
+				for playchild in child:
+					try:
+						if(playchild.name == 'span'):
+							for spanchild in playchild:
+								arow.append(spanchild.string)
+							elif(playchild['class'][0] == 'team-logo'):
+								arow.append(playchild['title'])
+					except:
+						continue
+			elif(child['class'][0] == 'period'):
+				tmpperiod = child.string
+			elif(child['class'][0] == 'clock'):          
+				csv.writer(results).writerow(arow)         
+				arow[:] = []
+				arow.append(tmpperiod)
+				arow.append(child.string)
 		except:
 			continue
+results.close()
+
+
+
+
+
+
+		
+
 			
 
