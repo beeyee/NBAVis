3c6eb374136e9185646b2cec594ad0f3cd597882
beeyee <paula-bf@163.com>
2014-03-13 01:56:06 +0800

    correct some erroes in text_live_parser.py

--- text_live_parser.py
+++ text_live_parser.py
@@ -15,61 +15,58 @@ divs = html.find_all('div')
 
 # in all of the div tags, find the play by play part.
 for elem in divs:
-    try:
+	try:
 		if(elem['id'] == 'mediasportsplaybyplay'):
-		# print elem
-			play_by_play = elem;	
+			# print elem
+			play_by_play = elem;
 	except KeyError:
 		continue
-		
+
 # get the play part		
 for child in play_by_play:
-    try:
+	try:
 		if(child['class'] == ['bd']):
-			plays = child
+			plays = child;
 	except:
 		continue
-		
+
+
 for child in plays:
 	try:
 		if(child.name == 'dl'):
 			real_play = child
 	except:
 		continue
-				
 
-results = open('./results.csv', 'w')
-wholelist = list()
-arow = list();
-	for child in real_play:
-		try:
-			if(child['class'][0] == 'play'):
-				for playchild in child:
-					try:
-						if(playchild.name == 'span'):
-							for spanchild in playchild:
+results = open('./files/play_by_play_file/results.csv', 'wb')
+wholelist = []
+arow = [];
+for child in real_play:
+	try:
+		if(child['class'][0] == 'play'):
+			for playchild in child:
+				try:
+					if(playchild.name == 'span'):
+						for spanchild in playchild:
+							if(spanchild.string != '\r\n'):
 								arow.append(spanchild.string)
-							elif(playchild['class'][0] == 'team-logo'):
-								arow.append(playchild['title'])
-					except:
-						continue
-			elif(child['class'][0] == 'period'):
-				tmpperiod = child.string
-			elif(child['class'][0] == 'clock'):          
-				csv.writer(results).writerow(arow)         
-				arow[:] = []
-				arow.append(tmpperiod)
+					elif(playchild['class'][0] == 'team-logo'):
+						arow.append(playchild['title'])
+				except:
+					continue
+		elif(child['class'][0] == 'period'):
+			tmpperiod = child.string
+		elif(child['class'][0] == 'clock'):
+			#print arow
+			#csv.writer(results).writerow(arow)
+			wholelist.append(list(arow))
+			arow[:] = []
+			arow.append(tmpperiod)
+			if(child.string != '\r\n'):
 				arow.append(child.string)
-		except:
-			continue
+	except:
+		continue
+csv.writer(results).writerows(wholelist)
 results.close()
 
-
-
-
-
-
-		
-
-			
 
